7f1ee48ec6c78f3a2a63a16ccf1224c8a7ebcf6b
Zero <zero@cock.li>
2022-11-05 04:50:02 -0400

    Fix for API change and add config file

--- 2hu_rabbits.sh
+++ 2hu_rabbits.sh
@@ -4,15 +4,24 @@
 # Needs curl, jq, wget
 
 # https://github.com/animeavi/fedi_ebooks/blob/master/auth_helper.rb
-access_token="xaxaxaxaxaxaxa"
-instance="example.com"
-img_dir="tmp"
-# public, unlisted, private
-visibility="unlisted"
+
+cd "${0%/*}"
+
+get_conf(){
+    str=$(grep "$1" config)
+    echo ${str#$1=}
+}
+
+access_token=$(get_conf access_token)
+instance=$(get_conf instance)
+img_dir=$(get_conf img_dir)
+post_text=$(get_conf post_text)
+visibility=$(get_conf visibility) # public, unlisted, private
+
 querybase="https://danbooru.donmai.us/posts/random.json?tags="
 
 # URL encode this (ie copy from a danbooru search)
-extra_param="+rating%3asafe+filetype:jpg,png" 
+extra_param="+rating%3asafe"
 declare -a Rabbits=( "reisen_udongein_inaba" "inaba_tewi" "ringo_%28touhou%29" "seiran_%28touhou%29" "reisen_%28touhou_bougetsushou%29" )
 declare -a ImageIds=()
 
@@ -21,24 +30,27 @@ for rabbit in ${Rabbits[@]}; do
    json=$(curl "$query" 2> /dev/null)
    imageurl=$(jq -r ".file_url" <<< "$json")
    [ -d "$img_dir" ] || mkdir "$img_dir" || (echo "Error creating image folder" && exit)
-   wget "$imageurl" -P "$img_dir" 2> /dev/null || (echo "Could not download image" && exit)
 
-   image_json=$( \
-	   curl -X POST "https://$instance/api/v1/media" \
-	   -H "Authorization: Bearer $access_token" \
-	   -F "file=@$img_dir/$(basename "$imageurl")" \
-	   -F "description=$(basename "$imageurl")" 2> /dev/null \
-   )
+   if [[ $imageurl == *jpg ]] || [[ $imageurl == *jpeg ]] || [[ $imageurl == *png ]] || [[ $imageurl == *gif ]]; then
+        wget "$imageurl" -P "$img_dir" 2> /dev/null || (echo "Could not download image" && exit)
 
-   id=$(jq -r ".id" <<< "$image_json")
-   
-   ImageIds+=($id)
+        image_json=$( \
+            curl -X POST "https://$instance/api/v1/media" \
+            -H "Authorization: Bearer $access_token" \
+            -F "file=@$img_dir/$(basename "$imageurl")" \
+            -F "description=$(basename "$imageurl")" 2> /dev/null \
+        )
+
+        id=$(jq -r ".id" <<< "$image_json")
+        ImageIds+=($id)
+   fi
+
    sleep 1
 done
 
 ids_json=$(jq --compact-output --null-input '$ARGS.positional' --args "${ImageIds[@]}")
 
-curl -X POST -d '{"status":":gyate_reisen_love:", "visibility":"'"$visibility"'", "media_ids":'"$ids_json"'}' \
+curl -X POST -d '{"status":"'"$post_text"'", "visibility":"'"$visibility"'", "media_ids":'"$ids_json"'}' \
  -H "Authorization: Bearer $access_token" \
  -H "Content-Type: application/json" \
  "https://$instance/api/v1/statuses"
